efe5e52b87c97f629ec753ae2b4952bfa0308ad3
jrsguitar <jeffreisouza@gmail.com>
2022-06-30 19:50:18 -0300

    Cria projeto

---
+++ src/main/java/br/com/jrcode/ApchePoi2Application.java
@@ -0,0 +1,13 @@
+package br.com.jrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApchePoi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApchePoi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jrcode/ApchePoi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApchePoi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
